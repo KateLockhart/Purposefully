b5a38bdcdbdbdc0380f858f2f0fa93c3580e82d7
KateLockhart <katelockhart707@gmail.com>
2020-10-04 14:54:35 -0400

    Data layer classes completed for Profile, Goal, and Entry.

---
+++ Purposefully.Data/Entry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purposefully.Data
+{
+    //Note: The journal entries of this program are titled "entry" for convience and efficientcy.
+    public class Entry
+    {
+        [Key]
+        public int EntryId { get; set; }
+        [Required]
+        public Guid Author { get; set; }
+        [Required]
+        public string EntryTitle { get; set; }
+        [Required]
+        public string EntryContent { get; set; }
+
+        [Required]
+        [Display(Name = "Is this for one of your goals?")]
+        public bool ForGoal { get; set; }
+        
+        [ForeignKey(nameof(Goal))]
+        public string GoalTitle { get; set; }
+        public virtual Goal Goal { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
---
+++ Purposefully.Data/Profile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purposefully.Data
+{
+    public class Profile
+    {
+        [Key]
+        public int ProfileId { get; set; }
+
+        [Required(ErrorMessage = "Please write your first name.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "First name should be between 3 and 20 characters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please write your last name.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "First name should be between 3 and 20 characters.")]
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+
+        [StringLength(500, MinimumLength = 0, ErrorMessage = "I'm sorry, your motivation section cannot be over 500 characters.")]
+        public string Motivation { get; set; }
+        public virtual ICollection<Goal> GoalsOfUser { get; set; } = new List<Goal>();
+
+    }
+}
